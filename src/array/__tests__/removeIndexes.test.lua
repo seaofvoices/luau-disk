ab53748dbcf3d386af7040f5f45ad59668367163
jeparlefrancais <35781636+jeparlefrancais@users.noreply.github.com>
2024-01-29 01:06:40 -0500

    Add `Array.removeIndexes` and `Array.removeSortedIndexes` (#3)

---
+++ src/array/__tests__/removeIndexes.test.lua
@@ -0,0 +1,95 @@
+local jestGlobals = (require)('@pkg/jest-globals')
+local removeIndexes = require('../removeIndexes')
+
+local expect = jestGlobals.expect
+local it = jestGlobals.it
+
+it('returns the same array when empty', function()
+    local empty = {}
+    local result = removeIndexes(empty, { 1, 3 })
+
+    expect(result).toBe(empty)
+end)
+
+it('returns the same array when given empty indexes array', function()
+    local array = { 'a', 'b', 'c', 'd' }
+    local result = removeIndexes(array, {})
+
+    expect(result).toBe(array)
+end)
+
+it('returns the same array when given indexes out of bounds', function()
+    local array = { 'a', 'b', 'c', 'd' }
+    local result = removeIndexes(array, { 7, 9, 5 })
+
+    expect(result).toBe(array)
+end)
+
+it('removes a single element from the array', function()
+    local array = { 'a', 'b', 'c', 'd' }
+    local result = removeIndexes(array, { 2 })
+
+    expect(result).toEqual({ 'a', 'c', 'd' })
+end)
+
+it('removes multiple elements from the array', function()
+    local array = { 'a', 'b', 'c', 'd', 'e', 'f' }
+    local result = removeIndexes(array, { 2, 3, 6 })
+
+    expect(result).toEqual({ 'a', 'd', 'e' })
+end)
+
+it('removes multiple elements from the array not in order', function()
+    local array = { 'a', 'b', 'c', 'd', 'e', 'f' }
+    local result = removeIndexes(array, { 6, 3, 2 })
+
+    expect(result).toEqual({ 'a', 'd', 'e' })
+end)
+
+it('removes indexes with duplicates', function()
+    local array = { 'a', 'b', 'c', 'd', 'e' }
+    local result = removeIndexes(array, { 2, 2, 4 })
+
+    expect(result).toEqual({ 'a', 'c', 'e' })
+end)
+
+it('removes index within the array but skips indexes exceeding array length', function()
+    local array = { 'a', 'b', 'c', 'd' }
+    local result = removeIndexes(array, { 1, 5, 8 })
+
+    expect(result).toEqual({ 'b', 'c', 'd' })
+end)
+
+it(
+    'removes indexes within the array but skips indexes exceeding array length not in order',
+    function()
+        local array = { 'a', 'b', 'c', 'd' }
+        local result = removeIndexes(array, { 2, 1, 8, 7 })
+
+        expect(result).toEqual({ 'c', 'd' })
+    end
+)
+
+it('handles mixed data types in the array', function()
+    type Mixed = { number | string | boolean }
+    local array: Mixed = { 1, 'hello', true, 'world', 42 }
+    local result = removeIndexes(array, { 2, 4 })
+
+    expect(result).toEqual({ 1, true, 42 } :: Mixed)
+end)
+
+it('returns an empty array when all indexes are removed', function()
+    local array = { 'a', 'b', 'c', 'd' }
+    local result = removeIndexes(array, { 1, 2, 3, 4 })
+
+    expect(result).toEqual({})
+end)
+
+it('returns an empty array when all indexes are removed not in order', function()
+    local array = { 'a', 'b', 'c', 'd' }
+    local result = removeIndexes(array, { 2, 1, 4, 3 })
+
+    expect(result).toEqual({})
+end)
+
+return nil
