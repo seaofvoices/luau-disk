48e66edb12ddd33ba0adcae8337ba3b8b6fcc69b
jeparlefrancais <jeparlefrancais21@gmail.com>
2024-03-03 13:31:15 -0500

    remove unused variable

--- src/array/contains.lua
+++ src/array/contains.lua
@@ -1,5 +1,5 @@
 local function contains<T>(array: { T }, element: T): boolean
-    for index, value in array do
+    for _, value in array do
         if value == element then
             return true
         end
