dc597ed12d0721cabb5d8a1646a6615bf7b9b967
jeparlefrancais <jeparlefrancais21@gmail.com>
2026-03-19 16:29:04 -0400

    Fix Array.flatten to respect the depth parameter

--- src/array/__tests__/flatten.test.lua
+++ src/array/__tests__/flatten.test.lua
@@ -38,4 +38,22 @@ it('flattens multiple levels of empty arrays', function()
     expect(result).toEqual({})
 end)
 
+it('flattens one level of nested arrays', function()
+    local result = flatten({ 1, { 2, { 3, 4 } :: any }, 5 }, 1)
+    expect(result).toEqual({ 1, 2, { 3, 4 } :: any, 5 })
+end)
+
+it('flattens two levels of nested arrays', function()
+    local deepNestedArray = { 1 :: any, { 2 :: any, { 3, { 4, 5 } :: any, 6 }, 7 }, 8 }
+    local resultWithDepth = flatten(deepNestedArray, 2)
+
+    expect(resultWithDepth).toEqual({ 1, 2, 3, { 4, 5 } :: any, 6, 7, 8 })
+end)
+
+it('returns the original array if depth is 0', function()
+    local value = { { 1 }, { 2 } }
+    local result = flatten(value, 0)
+    expect(result).toBe(value)
+end)
+
 return nil
