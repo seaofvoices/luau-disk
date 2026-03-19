a29ca22895c2d67228f65720b59996d44a359306
jeparlefrancais <35781636+jeparlefrancais@users.noreply.github.com>
2026-03-19 17:44:31 -0400

    Migrate project to use luau file extension (#36)

--- scripts/build-single-file.sh
+++ scripts/build-single-file.sh
@@ -9,11 +9,11 @@ mkdir -p roblox
 cp -r src/ roblox/
 
 find roblox/src -name '__tests__' -type d -exec rm -r {} +
-find roblox/src -name '*.test.lua' -type f -exec rm -r {} +
-find roblox/src -name 'jest.config.lua' -type f -exec rm -r {} +
+find roblox/src -name '*.test.luau' -type f -exec rm -r {} +
+find roblox/src -name 'jest.config.luau' -type f -exec rm -r {} +
 
 mkdir -p build
 
-rm -f build/disk.lua
+rm -f build/disk.luau
 
-darklua process --config .darklua-bundle.json roblox/src/init.lua build/disk.lua
+darklua process --config .darklua-bundle.json roblox/src/init.luau build/disk.luau
